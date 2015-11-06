d5722bf7e56190d72eed4bfd34273dedf74b0eba
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-11-06 18:58:34 +0900

    vm

--- t/c/re.c
+++ t/c/re.c
@@ -2,45 +2,141 @@
 
 #define RE_TRACE(fmt, ...) printf("# [pone] "); printf(fmt, ##__VA_ARGS__); printf("\n");
 
-#define RE(code) pone_val* re = pone_regex_new(world->universe, code);
-#define MATCH(s) RE_TRACE("'%s' ~~", s); pone_val* match = pone_call_method(world, re, "ACCEPTS", 1, pone_str_new_const(world->universe, s, strlen(s)));
+#define MATCH(s) RE_TRACE("'%s' ~~", s); pone_val* match = pone_re_match(world, prog, s, strlen(s));
 #define FROM() pone_intify(world, pone_call_method(world, match, "from", 0))
 #define TO() pone_intify(world, pone_call_method(world, match, "to", 0))
 #define FROM_IS(n) assert(FROM() == n)
 #define TO_IS(n) assert(TO() == n)
 #define NON_MATCH() assert(!pone_defined(match))
 
+// https://swtch.com/~rsc/regexp/regexp2.html
+
+#define MAXTHREADS 1000
+
+typedef enum pone_re_opcode {
+    PONE_RE_CAPTURE_START,
+    PONE_RE_CAPTURE_END,
+    PONE_RE_HEAD,
+    PONE_RE_TAIL,
+    PONE_RE_CHAR,
+    PONE_RE_MATCH,
+    PONE_RE_JMP,
+    PONE_RE_SPLIT
+} pone_re_opcode;
+
+typedef struct pone_re_inst {
+    pone_re_opcode opcode;
+    int c;
+    struct pone_re_inst *x;
+    struct pone_re_inst *y;
+} pone_re_inst;
+
+typedef struct pone_re_thread {
+    pone_re_inst* pc;
+    const char* sp;
+} pone_re_thread;
+
 // -----------------------------------------------------------------------------------------
 
-// /hoge/
-static pone_val* pone_regex_1(pone_world* world, pone_val* self, int n, va_list args) {
-    assert(n == 1);
-    int from = 0;
+static pone_val* match(pone_world* world, pone_re_inst* prog, const char* input, int len, int offset) {
+    pone_re_thread ready[MAXTHREADS];
+    pone_re_thread init = { /* create initial thread */
+        .pc = prog,
+        .sp = input
+    };
+    pone_re_inst* pc;
+    const char* sp;
+    ready[0] = init;
+    int nready = 1;
+    while (nready--) {
+        pc = ready[nready].pc;
+        sp = ready[nready].sp;
 
-    pone_val* str = va_arg(args, pone_val*);
-    const char* orig = pone_str_ptr(str);
-    const char* p;
-    int l = pone_str_len(str);
-
-START:
-    p = orig + from;
-    if (memcmp(p, "hoge", 4) == 0) {
-        int to = from + 4;
-        return pone_match_new(world->universe, from, to);
+        for (;;) {
+            switch (pc->opcode) {
+            case PONE_RE_HEAD:
+                RE_TRACE("  pc: %d head", pc-prog);
+                if (offset > 0) {
+                    goto Dead;
+                }
+                pc++;
+                continue;
+            case PONE_RE_TAIL:
+                RE_TRACE("  pc: %d tail", pc-prog);
+                // RE_TRACE("len:%d!=%d, input:%x, sp:%x", len, sp-input, input, sp);
+                if (sp-input != len) {
+                    goto Dead;
+                }
+                pc++;
+                continue;
+            case PONE_RE_CHAR:
+                RE_TRACE("  pc: %d char", pc-prog);
+                if (*sp != pc->c) {
+                    goto Dead;
+                }
+                pc++;
+                sp++;
+                continue;
+            case PONE_RE_MATCH:
+                RE_TRACE("  pc: %d match", pc-prog);
+                return pone_match_new(world->universe, offset, sp-input+offset);
+            case PONE_RE_JMP:
+                RE_TRACE("  pc: %d jmp", pc-prog);
+                pc = pc->x;
+                continue;
+            case PONE_RE_CAPTURE_START:
+                // match 1
+                pc++;
+                continue;
+            case PONE_RE_CAPTURE_END:
+                pc++;
+                continue;
+            case PONE_RE_SPLIT: {
+                RE_TRACE("  pc: %d split", pc-prog);
+                if (nready >= MAXTHREADS) {
+                    fprintf(stderr, "regexp overflow");
+                    abort();
+                }
+                /* queue new thread */
+                pone_re_thread thr = {
+                    .pc = pc->y,
+                    .sp = sp
+                };
+                ready[nready++] = thr;
+                pc = pc->x;  /* continue current thread */
+                continue;
+            }
+            }
+            abort();
+        }
+Dead:;
     }
-
-    from++;
-    if (from == l) {
-        RE_TRACE("reached to end");
-        return pone_nil();
-    } else {
-        RE_TRACE("next. %d", from);
-        goto START;
-    }
+    return pone_nil();
 }
 
+pone_val* pone_re_match(pone_world* world, pone_re_inst* prog, const char* str, size_t len) {
+    for (size_t i=0; i<len; ++i) {
+        pone_val* v = match(world, prog, str+i, len-i, i);
+        if (pone_defined(v)) {
+            return v;
+        }
+    }
+    return pone_nil();
+}
+
+// /hoge/
 static void run_tests1(pone_world* world) {
-    RE(pone_regex_1);
+    pone_re_inst prog[5];
+    prog[0].opcode = PONE_RE_CHAR;
+    prog[0].c      = 'h';
+    prog[1].opcode = PONE_RE_CHAR;
+    prog[1].c      = 'o';
+    prog[2].opcode = PONE_RE_CHAR;
+    prog[2].c      = 'g';
+    prog[3].opcode = PONE_RE_CHAR;
+    prog[3].c      = 'e';
+    prog[4].opcode = PONE_RE_MATCH;
+
     {
         MATCH("hoge");
         FROM_IS(0);
@@ -59,42 +155,20 @@ static void run_tests1(pone_world* world) {
     }
 }
 
-// -----------------------------------------------------------------------------------------
-
-// /^hoge/
-static pone_val* pone_regex_2(pone_world* world, pone_val* self, int n, va_list args) {
-    assert(n == 1);
-    int from = 0;
-
-    pone_val* str = va_arg(args, pone_val*);
-    const char* p = pone_str_ptr(str);
-    int l = pone_str_len(str);
-
-START:
-    // ^
-    if (from != 0) {
-        return pone_nil();
-    }
-
-    // hoge
-    if (memcmp(p+from, "hoge", 4) == 0) {
-        int to = from + 4;
-        return pone_match_new(world->universe, from, to);
-    }
-
-    from++;
-    if (from == l) {
-        RE_TRACE("reached to end");
-        return pone_nil();
-    } else {
-        RE_TRACE("next. %d", from);
-        goto START;
-    }
-}
-
 // /^hoge/
 static void run_tests2(pone_world* world) {
-    RE(pone_regex_2);
+    int n = 0;
+    pone_re_inst prog[6];
+    prog[n++].opcode = PONE_RE_HEAD;
+    prog[n].opcode   = PONE_RE_CHAR;
+    prog[n++].c      = 'h';
+    prog[n].opcode   = PONE_RE_CHAR;
+    prog[n++].c      = 'o';
+    prog[n].opcode   = PONE_RE_CHAR;
+    prog[n++].c      = 'g';
+    prog[n].opcode   = PONE_RE_CHAR;
+    prog[n++].c      = 'e';
+    prog[n++].opcode = PONE_RE_MATCH;
 
     {
         MATCH("hoge");
@@ -119,55 +193,21 @@ static void run_tests2(pone_world* world) {
 // -----------------------------------------------------------------------------------------
 
 // /^hoge$/
-static pone_val* pone_regex_3(pone_world* world, pone_val* self, int n, va_list args) {
-    assert(n == 1);
-    int from = 0;
-    int to = 0;
 
-    pone_val* str = va_arg(args, pone_val*);
-    const char* orig = pone_str_ptr(str);
-    const char* p;
-    int l = pone_str_len(str);
-
-START:
-    to = from;
-    p = orig + from;
-
-    // ^
-    if (from != 0) {
-        return pone_nil();
-    }
-
-    // hoge
-    if (memcmp(p, "hoge", 4) == 0) {
-        to += 4;
-    } else {
-        goto NEXT;
-    }
-
-    // $
-    RE_TRACE("'$' %d != %d", to, l);
-    if (to != l) {
-        return pone_nil();
-    }
-
-    // succeeded
-    return pone_match_new(world->universe, from, to);
-
-NEXT:
-    from++;
-    if (from == l) {
-        RE_TRACE("reached to end");
-        return pone_nil();
-    } else {
-        RE_TRACE("next. %d", from);
-        goto START;
-    }
-}
-
-// /^hoge$/
 static void run_tests3(pone_world* world) {
-    RE(pone_regex_3);
+    int n = 0;
+    pone_re_inst prog[6];
+    prog[n++].opcode = PONE_RE_HEAD;
+    prog[n].opcode   = PONE_RE_CHAR;
+    prog[n++].c      = 'h';
+    prog[n].opcode   = PONE_RE_CHAR;
+    prog[n++].c      = 'o';
+    prog[n].opcode   = PONE_RE_CHAR;
+    prog[n++].c      = 'g';
+    prog[n].opcode   = PONE_RE_CHAR;
+    prog[n++].c      = 'e';
+    prog[n++].opcode = PONE_RE_TAIL;
+    prog[n++].opcode = PONE_RE_MATCH;
 
     {
         MATCH("hoge");
@@ -180,72 +220,59 @@ static void run_tests3(pone_world* world) {
     }
 }
 
-// -----------------------------------------------------------------------------------------
+// /e$/
+static void run_tests4(pone_world* world) {
+    int n = 0;
+    pone_re_inst prog[3];
+    prog[n  ].c      = 'e';
+    prog[n++].opcode = PONE_RE_CHAR;
+    prog[n++].opcode = PONE_RE_TAIL;
+    prog[n++].opcode = PONE_RE_MATCH;
 
-// /^ hog e* $/
-static pone_val* pone_regex_4(pone_world* world, pone_val* self, int n, va_list args) {
-    assert(n == 1);
-    int from = 0;
-    int to = 0;
-
-    pone_val* str = va_arg(args, pone_val*);
-    const char* orig_p = pone_str_ptr(str);
-    const char* p;
-    int l = pone_str_len(str);
-
-START:
-    to = from;
-    p = orig_p + from;
-
-    // ^
-    if (from != 0) {
-        return pone_nil();
+    {
+        MATCH("hoge");
+        FROM_IS(3);
+        TO_IS(4);
     }
-
-    // hog
-    if (memcmp(p, "hog", 3) == 0) {
-        p += 3;
-        to += 3;
-    } else {
-        RE_TRACE("/hog/ match failed");
-        goto NEXT;
-    }
-
-    // e*
-    while (to!=l) {
-        RE_TRACE("matching /e*/ %d != %d", to, l);
-        if (memcmp(p, "e", 1) == 0) {
-            p++;
-            to++;
-        } else {
-            RE_TRACE("%c != 'e'", *p);
-            break;
-        }
-    }
-
-    // $
-    RE_TRACE("'$' %d != %d", to, l);
-    if (to != l) {
-        return pone_nil();
-    }
-
-    // succeeded
-    return pone_match_new(world->universe, from, to);
-
-NEXT:
-    from++;
-    if (from == l) {
-        RE_TRACE("reached to end");
-        return pone_nil();
-    } else {
-        RE_TRACE("next. %d", from);
-        goto START;
+    {
+        MATCH("hogeea");
+        NON_MATCH();
     }
 }
 
-// /^hoge$/
-static void run_tests4(pone_world* world) {
-    RE(pone_regex_4);
+// /^ hog e* $/
+static void run_tests5(pone_world* world) {
+    // L1: split L2, L3
+    // L2: codes for e
+    //     jmp L1
+    //     L3:
+
+    int n = 0;
+    pone_re_inst prog[8];
+    prog[n  ].c      = 'h';
+    prog[n++].opcode = PONE_RE_CHAR;
+    prog[n  ].c      = 'o';
+    prog[n++].opcode = PONE_RE_CHAR;
+    prog[n  ].c      = 'g';
+    prog[n++].opcode = PONE_RE_CHAR;
+
+    pone_re_inst *l1 = &(prog[n]);
+    prog[n++].opcode = PONE_RE_SPLIT;
+
+    // L2:
+    pone_re_inst *l2 = &(prog[n]);
+    prog[n  ].c      = 'e';
+    prog[n++].opcode = PONE_RE_CHAR;
+    l1->x = l2;
+
+    prog[n  ].x      = l1;
+    prog[n++].opcode = PONE_RE_JMP;
+
+    pone_re_inst *l3 = &(prog[n]);
+    l1->y = l3;
+    prog[n++].opcode = PONE_RE_TAIL;
+
+    prog[n++].opcode = PONE_RE_MATCH;
 
     {
         MATCH("hog");
@@ -264,6 +291,89 @@ static void run_tests4(pone_world* world) {
     }
     {
         MATCH("hogeem");
+        NON_MATCH();
+    }
+}
+
+// /^ hog e* m $/
+static void run_tests6(pone_world* world) {
+    // L1: split L2, L3
+    // L2: codes for e
+    //     jmp L1
+    //     L3:
+
+    int n = 0;
+    pone_re_inst prog[9];
+    prog[n  ].c      = 'h';
+    prog[n++].opcode = PONE_RE_CHAR;
+    prog[n  ].c      = 'o';
+    prog[n++].opcode = PONE_RE_CHAR;
+    prog[n  ].c      = 'g';
+    prog[n++].opcode = PONE_RE_CHAR;
+
+    pone_re_inst *l1 = &(prog[n]);
+    prog[n++].opcode = PONE_RE_SPLIT;
+
+    // L2:
+    pone_re_inst *l2 = &(prog[n]);
+    prog[n  ].c      = 'e';
+    prog[n++].opcode = PONE_RE_CHAR;
+    l1->x = l2;
+
+    prog[n  ].x      = l1;
+    prog[n++].opcode = PONE_RE_JMP;
+
+    pone_re_inst *l3 = &(prog[n]);
+    l1->y = l3;
+
+    prog[n  ].c      = 'm';
+    prog[n++].opcode = PONE_RE_CHAR;
+
+    prog[n++].opcode = PONE_RE_TAIL;
+
+    prog[n++].opcode = PONE_RE_MATCH;
+
+    {
+        MATCH("hogm");
+        FROM_IS(0);
+        TO_IS(4);
+    }
+    {
+        MATCH("hogem");
+        FROM_IS(0);
+        TO_IS(5);
+    }
+    {
+        MATCH("hogeeem");
+        FROM_IS(0);
+        TO_IS(7);
+    }
+    {
+        MATCH("hogeea");
+        NON_MATCH();
+    }
+}
+
+// /^(e)$/
+static void run_tests7(pone_world* world) {
+    int n=0;
+    pone_re_inst prog[6];
+    prog[n++].opcode = PONE_RE_HEAD;
+    prog[n++].opcode = PONE_RE_CAPTURE_START;
+    prog[n  ].c      = 'e';
+    prog[n++].opcode = PONE_RE_CHAR;
+    prog[n++].opcode = PONE_RE_CAPTURE_END;
+    prog[n++].opcode = PONE_RE_TAIL;
+    prog[n++].opcode = PONE_RE_MATCH;
+
+    {
+        MATCH("e");
+        FROM_IS(0);
+        TO_IS(1);
+    }
+
+    {
+        MATCH("uwaa");
         NON_MATCH();
     }
 }
@@ -287,8 +397,14 @@ int main(int argc, char** argv) {
         run_tests2(world);
         RE_TRACE("/^hoge$/");
         run_tests3(world);
-        RE_TRACE("/^hog e* $/");
+        RE_TRACE("/e$/");
         run_tests4(world);
+        RE_TRACE("/^hog e* $/");
+        run_tests5(world);
+        RE_TRACE("/^hog e* m $/");
+        run_tests6(world);
+        RE_TRACE("/^ (e) $/");
+        run_tests7(world);
 
         pone_freetmps(world);
         pone_pop_scope(world);
