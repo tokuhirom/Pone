4c25d2a432857aace6499287020d876c03e81240
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-11-11 08:31:57 +0900

    Added Pair#Str

--- src/pair.c
+++ src/pair.c
@@ -11,6 +11,15 @@ pone_val* pone_pair_new(pone_universe* universe, pone_val* key, pone_val* value)
 PONE_DECLARE_GETTER(meth_pair_key, "$!key")
 PONE_DECLARE_GETTER(meth_pair_value, "$!value")
 
+static pone_val* meth_pair_str(pone_world* world, pone_val* self, int n, va_list args) {
+    assert(n==0);
+
+    pone_val* buf = pone_str_copy(world->universe, pone_stringify(world, pone_obj_get_ivar(world->universe, self, "$!key")));
+    pone_str_append_c(world, buf, "\t", 1);
+    pone_str_append(world, buf, pone_obj_get_ivar(world->universe, self, "$!value"));
+    return buf;
+}
+
 void pone_pair_init(pone_universe* universe) {
     assert(universe->class_pair == NULL);
 
@@ -18,6 +27,7 @@ void pone_pair_init(pone_universe* universe) {
     pone_class_push_parent(universe, universe->class_pair, universe->class_any);
     pone_add_method_c(universe, universe->class_pair, "key", strlen("key"), meth_pair_key);
     pone_add_method_c(universe, universe->class_pair, "value", strlen("value"), meth_pair_value);
+    pone_add_method_c(universe, universe->class_pair, "Str", strlen("Str"), meth_pair_str);
     pone_class_compose(universe, universe->class_pair);
     pone_universe_set_global(universe, "Pair", universe->class_pair);
 }
