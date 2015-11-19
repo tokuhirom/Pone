d130052e31772512cc4b717a28f445fca63977f2
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-11-19 19:56:43 +0900

    remove NODE_MULTI

--- src/pvip.h
+++ src/pvip.h
@@ -116,7 +116,6 @@ typedef enum {
     PVIP_NODE_STRINGIFY, /* prefix:<~> */
     PVIP_NODE_TRY,
     PVIP_NODE_REF,
-    PVIP_NODE_MULTI,
     PVIP_NODE_UNARY_BOOLEAN, /* ? */
     PVIP_NODE_UNARY_UPTO, /* ^ */
     PVIP_NODE_STDOUT, /* $*OUT */
