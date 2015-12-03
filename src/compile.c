cdf52e4241e2ca7f4672d8ff9e7e5eeec1a14159
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-12-03 20:31:37 +0900

    use destination directory as -I path

--- src/compile.c
+++ src/compile.c
@@ -1350,10 +1350,9 @@ pone_node* pone_parse_fp(pone_world* world, pvip_t* pvip, FILE* fp, bool yy_debu
 }
 
 void pone_compile_c(pone_world* world, const char* so_filename, const char* c_filename) {
-    pone_val* cmdline = pone_str_c_str(world, pone_str_new_printf(world, "clang -x c -D_POSIX_C_SOURCE=200809L -rdynamic -fPIC -shared -Iinclude/ -I src/ -g -lm -std=c99 -o %s %s -L. -lpone", so_filename, c_filename));
+    pone_val* cmdline = pone_str_c_str(world, pone_str_new_printf(world, "clang -x c -D_POSIX_C_SOURCE=200809L -rdynamic -fPIC -shared -I%s -I src/ -g -lm -std=c99 -o %s %s -L. -lpone", PONE_INCLUDE_DIR, so_filename, c_filename));
     int retval = system(pone_str_ptr(cmdline));
     if (retval != 0) {
         pone_throw_str(world, "cannot compile generated C code");
     }
 }
-
